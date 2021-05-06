8955d7c5522f1aa1530059f22f176b038074dc7f
Javier <mrvizious@gmail.com>
2021-05-06 19:45:54 +0200

    Arrow is now Vitae

--- Assets/Scripts/Player/Vitae/VitaeAnimation.cs
+++ Assets/Scripts/Player/Vitae/VitaeAnimation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ArrowAnimation : MonoBehaviour
+public class VitaeAnimation : MonoBehaviour
 {
     public PlayerData player;
     private void Update() {

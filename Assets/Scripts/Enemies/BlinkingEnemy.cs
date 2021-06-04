ebdae05b16a9247415c35d110f14cf07e6940191
Javier <mrvizious@gmail.com>
2021-06-05 01:01:18 +0200

    Room R 1 completed

--- Assets/Scripts/Enemies/BlinkingEnemy.cs
+++ Assets/Scripts/Enemies/BlinkingEnemy.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class BlinkingEnemy : SeekerEnemy
 {
     public float blinkingSpeed;
+    public SpriteRenderer healthBar, healthBackground;
+    private float offset;
     private SpriteRenderer spriteRenderer;
     private float timeSinceBeginning = 0f;
 
@@ -13,20 +15,29 @@ public class BlinkingEnemy : SeekerEnemy
         base.Start();
         timeSinceBeginning = 0f;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        offset = Random.Range(0f, 2f);
         StartCoroutine(BlinkingCoroutine());
     }
 
     private IEnumerator BlinkingCoroutine() {
         while (true)
         {
-            spriteRenderer.color = new Color(
-                spriteRenderer.color.r,
-                spriteRenderer.color.g,
-                spriteRenderer.color.b,
-                (Mathf.Cos(timeSinceBeginning * blinkingSpeed) + 1) / 2
-            );
+            float alphaValue = (Mathf.Cos((timeSinceBeginning + offset) * blinkingSpeed) + 1) / 2;
+            SetAlpha(spriteRenderer, alphaValue);
+            SetAlpha(healthBar, alphaValue);
+            SetAlpha(healthBackground, alphaValue);
+
             timeSinceBeginning += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
+    }
+
+    private void SetAlpha(SpriteRenderer sprite, float alphaValue) {
+        sprite.color = new Color(
+            sprite.color.r,
+            sprite.color.g,
+            sprite.color.b,
+            alphaValue
+        );
     }
 }
